3db61aa0b179fa67ca1859e0a59ebb1d495f3c44
Dunkan <danyakononenko14@gmail.com>
2025-07-28 14:26:30 +0300

    refac: add colors to the output data

--- src/main/java/com/example/services/Bin2DecService.java
+++ src/main/java/com/example/services/Bin2DecService.java
@@ -10,6 +10,10 @@ public class Bin2DecService {
   private final Convertor convertor;
   private final Validator validator;
 
+  private static final String RESET = "\u001B[0m";
+  private static final String RED = "\u001B[31m";
+  private static final String GREEN = "\u001B[32m";
+
   public Bin2DecService(Input input, Convertor convertor, Validator validator) {
     this.input = input;
     this.convertor = convertor;
@@ -38,12 +42,12 @@ public class Bin2DecService {
   }
 
   private void throwResult(int result) {
-    System.out.printf("\nThe result is: %d", result);
+    System.out.printf(GREEN + "\nThe result is: %d" + RESET, result);
   }
 
   private void throwError(String invalidInput) {
-    System.out.printf("\nInvalid input: %s"
-                          + "\nYour binary should:"
+    System.out.printf(RED + "\nInvalid input: %s" + RESET +
+                          "\nYour binary should:"
                           + "\n- have length of 8"
                           + "\n- contain only 0 and 1\n"
                           + "\nTry again.\n",
